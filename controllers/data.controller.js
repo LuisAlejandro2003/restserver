6bb0cbbb5c552a3d3218e285970c4fbb2924f17a
LuisAlejandro2003 <luisalejandro2003121822@gmail.com>
2023-11-07 11:17:50 -0600

    [UPDATE] socket suscription added

--- controllers/data.controller.js
+++ controllers/data.controller.js
@@ -1,4 +1,7 @@
 const { response, request } = require('express');
+const socketIo = require('socket.io-client');
+const socket = socketIo('http://localhost:3001'); // Reemplaza 'tu-servidor-socket' con la URL de tu servidor de sockets
+
 const Data = require('../models/data.model');
 
 const dataPost = async (req, res) => {
@@ -8,6 +11,10 @@ const dataPost = async (req, res) => {
 
     //guardar en db
     await data.save();
+
+    socket.emit('nuevos-datos', data);
+
+
     res.json({
         data
     });
